7d38404cd015151e8f4c2a62b884850d4d1f101c
aljosagames <aljosapaunovic555@gmail.com>
2023-03-05 17:29:58 +0100

    nav bar

---
+++ src/js/adminPage.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  // hamburger menu toggle
+  $(".nav-toggle").click(function () {
+    $(".main-nav").toggleClass("is-open");
+    $(".hamburger").toggleClass("is-open");
+  });
+
+  $(".btn-books").click(function () {
+    event.preventDefault();
+    $(".iBooks").toggleClass("fa-angle-down");
+    $(".iBooks").toggleClass("fa-angle-up");
+    $(".ulBooks").toggleClass("hidden");
+  });
+
+  $(".btn-acc").click(function () {
+    event.preventDefault();
+    $(".iAcc").toggleClass("fa-angle-down");
+    $(".iAcc").toggleClass("fa-angle-up");
+    $(".ulAcc").toggleClass("hidden");
+  });
+});
